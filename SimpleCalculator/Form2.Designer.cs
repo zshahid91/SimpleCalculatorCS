a1034af5a0bc4d1ef7793331c0ed0d5106fa2c2c
Zohaib Shahid <zohaib_1991@hotmail.com>
2021-04-01 21:37:55 +0500

    Started adding Number buttons in order to start building a more advanced version of the Calculator

--- SimpleCalculator/Form2.Designer.cs
+++ SimpleCalculator/Form2.Designer.cs
@@ -28,32 +28,169 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.num0 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // maskedTextBox1
+            // button1
             // 
-            this.maskedTextBox1.Location = new System.Drawing.Point(55, 12);
-            this.maskedTextBox1.Name = "maskedTextBox1";
-            this.maskedTextBox1.Size = new System.Drawing.Size(363, 50);
-            this.maskedTextBox1.TabIndex = 0;
-            this.maskedTextBox1.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.maskedTextBox1_MaskInputRejected);
+            this.button1.Location = new System.Drawing.Point(222, 506);
+            this.button1.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 37);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(15, 14);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(537, 62);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // num0
+            // 
+            this.num0.Location = new System.Drawing.Point(105, 358);
+            this.num0.Name = "num0";
+            this.num0.Size = new System.Drawing.Size(65, 56);
+            this.num0.TabIndex = 3;
+            this.num0.Text = "0";
+            this.num0.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(105, 296);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(65, 56);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "0";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(176, 296);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(65, 56);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "0";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(247, 296);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(65, 56);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "0";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(105, 234);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(65, 56);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "0";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(176, 234);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(65, 56);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "0";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(247, 234);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(65, 56);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "0";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(105, 172);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(65, 56);
+            this.button8.TabIndex = 10;
+            this.button8.Text = "0";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(176, 172);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(65, 56);
+            this.button9.TabIndex = 11;
+            this.button9.Text = "0";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(247, 172);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(65, 56);
+            this.button10.TabIndex = 12;
+            this.button10.Text = "0";
+            this.button10.UseVisualStyleBackColor = true;
             // 
             // Form2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(486, 413);
-            this.Controls.Add(this.maskedTextBox1);
+            this.ClientSize = new System.Drawing.Size(584, 461);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.num0);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Gadugi", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
             this.Name = "Form2";
-            this.Text = "Form2";
+            this.Text = "AdvCalculator";
             this.Load += new System.EventHandler(this.Form2_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button num0;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
     }
 }
--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -16,7 +16,7 @@ namespace SimpleCalculator
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form2());
         }
     }
 }
